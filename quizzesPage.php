3d473a658599f68c2815f5ea41055f43c5af24e1
ColeBelfry <cbelfry@student.neumont.edu>
2023-05-23 13:08:48 -0600

    added quizzes page

---
+++ quizzesPage.php
@@ -0,0 +1,47 @@
+<div class="navlinks">
+    <a>test 1</a>
+    <a>test 2</a>
+    <a>test 3</a>
+</div>
+<div class="body-container">
+    <div class="info-container">
+        <div class="gradient-banner">
+            <div>Quiztopia</div>
+        </div>
+        <div class="utility-bar">
+            <input class="search-bar" placeholder="search quizzes" />
+        </div>
+        <div class="quizzes-container">
+            <a class="quiz-card">
+                <div class="quiz-body">
+                    <div class="quiz-title">Quiz Name</div>
+                    <p>Description</p>
+                </div>
+            </a>
+            <a class="quiz-card">
+                <div class="quiz-body">
+                    <div class="quiz-title">Quiz Name</div>
+                    <p>Description</p>
+                </div>
+            </a>
+            <a class="quiz-card">
+                <div class="quiz-body">
+                    <div class="quiz-title">Quiz Name</div>
+                    <p>Description</p>
+                </div>
+            </a>
+            <a class="quiz-card">
+                <div class="quiz-body">
+                    <div class="quiz-title">Quiz Name</div>
+                    <p>Description</p>
+                </div>
+            </a>
+            <a class="quiz-card">
+                <div class="quiz-body">
+                    <div class="quiz-title">Quiz Name</div>
+                    <p>Description</p>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
