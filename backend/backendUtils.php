c43cd736ad5aedebf7a936e17ce590ff00e82e6a
Joseph Baker <joeyhbaker@gmail.com>
2023-05-24 09:38:09 -0600

    Backend Utility Methods

--- backend/backendUtils.php
+++ backend/backendUtils.php
@@ -8,9 +8,35 @@ function DBConnection() {
     return $con;
 }
 
+/**
+ * Execute insert, update, and delete query statements
+ * @param mixed $query Passed in query to run using mysqli
+ * @return string Result of the query execution
+ */
 function AffectedRowsExecution($query) {
     $dbConnection = DBConnection();
     $result = @mysqli_query($dbConnection, $query) . " row affected";
+    @mysqli_close($dbConnection);
+    return $result;
+}
+
+/**
+ * Execute select query statements
+ * @param mixed $query Passed in query to run using mysqli
+ * @return string Result of the query execution
+ */
+function SelectExecution($query) {
+    $dbConnection = DBConnection();
+    $dataSet = @mysqli_query($dbConnection, $query);
+    $result = "";
+
+    if ($dataSet) {
+        $rowArray = [];
+        while ($row = @mysqli_fetch_array($dataSet)) {
+            $rowArray[] = json_decode($row[0]);
+        }
+        $result = json_encode($rowArray);
+    }
     @mysqli_close($dbConnection);
     return $result;
 }
