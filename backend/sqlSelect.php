8dab27963db88e8ed744c87d0b3470db2cbfe7c2
Joseph Baker <joeyhbaker@gmail.com>
2023-05-26 12:12:15 -0600

    Added extra select by Id possibilities

--- backend/sqlSelect.php
+++ backend/sqlSelect.php
@@ -8,6 +8,9 @@ $json = '';
 
 // Are we reading from a table?
 if (array_key_exists("table", $_GET)) {
+    $body = file_get_contents('php://input');
+    $data = json_decode($body, true);
+    if ($data == null) $data = array();
     $destination = $_GET["table"];
 
     // Is it a valid table?
@@ -15,20 +18,20 @@ if (array_key_exists("table", $_GET)) {
         // Valid destinations: user, quiz, question
         switch ($destination) {
             case "user":
-                $json = userSelect();
+                $json = userSelect($data);
                 break;
             case "quiz":
-                $json = quizSelect();
+                $json = quizSelect($data);
                 break;
             case "question":
-                $json = questionSelect();
+                $json = questionSelect($data);
                 break;
             default:
                 $json = "Invalid Table";
         }
 
         // Check if we got data back?
-        if ($json == "null" || $json == "") 
+        if ($json == "null" || $json == "")
             $json = "No Data Found";
     } else {
         $json = "Invalid Table";
@@ -41,27 +44,41 @@ echo $json;
  * Retrieve all users
  * @return string Result of insertion
  */
-function userSelect() {
+function userSelect($data) {
     $user = new user();
-    return SelectExecution($user->SelectQuery());
+    if (array_key_exists("userId", $data)) {
+        return SelectExecution($user->SelectQuery(columns: array(), filters: array($user->CreateFilterExact("id", $data["userId"]))));
+    } else {
+        return SelectExecution($user->SelectQuery());
+    }
 }
 
 /**
  * Retrieve all quizzes
  * @return string Result of insertion
  */
-function quizSelect() {
+function quizSelect($data) {
     $quiz = new quiz();
-    return SelectExecution($quiz->SelectQuery());
+    if (array_key_exists("quizId", $data)) {
+        return SelectExecution($quiz->SelectQuery(columns: array(), filters: array($quiz->CreateFilterExact("id", $data["quizId"]))));
+    } else {
+        return SelectExecution($quiz->SelectQuery());
+    }
 }
 
 /**
  * Retrieve all questions
  * @return string Result of insertion
  */
-function questionSelect() {
+function questionSelect($data) {
     $question = new question();
-    return SelectExecution($question->SelectQuery());
+    if (array_key_exists("quizId", $data)) {
+        return SelectExecution($question->SelectQuery(columns: array(), filters: array($question->CreateFilterExact("quizId", $data["quizId"]))));
+    } else if (array_key_exists("questionId", $data)) {
+        return SelectExecution($question->SelectQuery(columns: array(), filters: array($question->CreateFilterExact("id", $data["questionId"]))));
+    } else {
+        return SelectExecution($question->SelectQuery());
+    }
 }
 #endregion
 ?>
