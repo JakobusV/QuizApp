03962830e804a42c6802a274158bb50126899056
JakobusV <51762364+JakobusV@users.noreply.github.com>
2023-05-30 20:46:18 -0600

    reverting models, play and profile php file changes

--- playQuiz.php
+++ playQuiz.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'header.php';
-include_once 'question.php';
 GenerateHeader("Play Quiz Page", ['playQuiz.css']);
+CanIBeHere();
 ?>
 <body>
     <?php
@@ -9,10 +9,131 @@ GenerateHeader("Play Quiz Page", ['playQuiz.css']);
     ?>
     <div class="body-container">
         <div class="info-container">
-            <?php
-                GenerateQuestionElement(1, "What is the Capital of Spain?", "Madrid", array('Florence', 'Paris', 'Mexico City'));
-            ?>
+            <div class="question-box">
+                <p>Quiz:</p>
+                <h2></h2>
+            </div>
+            <div class="options-container"></div>
+            <button class="option-btn" onclick="StartQuiz()">Start Quiz</button>
         </div>
     </div>
 </body>
+
+<script>
+    let questions = [];
+    let title = '';
+    let questionNumber = 0;
+    let points = 0;
+    let questionCounterP = document.querySelector('.question-box p');
+    let questionContentH2 = document.querySelector('.question-box h2');
+    let optionsContainer = document.querySelector('.options-container');
+
+    const SetupQuiz = async () => {
+        // get username from url param
+        var urlFormatParams = window.location.search;
+        var params = new URLSearchParams(urlFormatParams);
+        var quizId = params.get('q');
+        // fetch quiz data
+        const response = await fetch('backend/sqlJoin.php?join=questions', {
+            method: 'POST',
+            body: `{"quizId": "${quizId}"}`,
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const json = await response.json();
+        // clear format questions
+        json.forEach((data) => {
+            questions.push(CreateQuestionFormat(data));
+        });
+
+        title = json[0].title;
+        questionContentH2.innerHTML = title;
+    }
+
+    const StartQuiz = () => {
+        questionNumber = 0;
+        points = 0;
+
+        NextQuestion();
+        ToggleOptionButton();
+    }
+
+    const ToggleOptionButton = () => {
+        let optionBtn = document.getElementsByClassName('option-btn')[0];
+        if (optionBtn.style.display === "none") {
+            optionBtn.innerHTML = 'Restart Quiz';
+            optionBtn.style.display = "block";
+        }
+        else
+            optionBtn.style.display = "none";
+    }
+
+    const CreateQuestionFormat = (data) => {
+        let question = {
+            content: data.content,
+            r: data.rightAnswer,
+            w1: data.wrong1,
+            w2: data.wrong2,
+            w3: data.wrong3,
+        }
+        return question;
+    }
+
+    const NextQuestion = () => {
+        let index = questionNumber;
+        questionNumber++;
+        if (questionNumber > questions.length)
+            return EndQuiz();
+
+        let question = questions[index];
+
+        questionCounterP.innerHTML = `Question ${questionNumber}:`;
+        questionContentH2.innerHTML = question.content;
+
+        MakeQuestionSet(question);
+    }
+
+    const MakeQuestionSet = (question) => {
+        let answers = [];
+        answers.push(MakeQuestionButton(question.r));
+        answers.push(MakeQuestionButton(question.w1));
+        answers.push(MakeQuestionButton(question.w2));
+        answers.push(MakeQuestionButton(question.w3));
+        shuffle(answers);
+        optionsContainer.innerHTML = answers.join('\n');
+    }
+
+    const MakeQuestionButton = (answer) => {
+        return `<button class="answer-btn" onclick="ChosenAnswer('${answer}')">${answer}</button>`
+    }
+
+    const ChosenAnswer = (answer) => {
+        let currentQuestion = questions[questionNumber - 1];
+        if (answer == currentQuestion.r)
+            points++;
+        NextQuestion();
+    }
+
+    const EndQuiz = () => {
+        questionCounterP.innerHTML = 'Quiz Results:';
+        questionContentH2.innerHTML = `${title}`;
+        optionsContainer.innerHTML = `<h3>Final Score: ${points}/${questions.length}</h3>`;
+        ToggleOptionButton();
+    }
+
+    const shuffle = (array) => {
+        let arraySize = array.length;
+        let randomIndex = 0;
+        while (arraySize > 1) {
+            randomIndex = Math.floor(Math.random() * arraySize);
+            arraySize--;
+            let temp = array[arraySize];
+            array[arraySize] = array[randomIndex];
+            array[randomIndex] = temp;
+        }
+        return array;
+    }
+
+    SetupQuiz();
+
+</script>
 <?php
--- userPage.php
+++ userPage.php
@@ -1,6 +1,19 @@
 <?php
 include_once 'header.php';
+include_once 'utility.php';
+include_once 'backend/backendUtils.php';
+include_once 'backend/models.php';
 GenerateHeader('User Page', ['userPage.css']);
+CanIBeHere();
+$myPage = false;
+$username = '';
+
+if (isset($_GET['u']))
+    $userSession = GetSession('current_user');
+if (isset($userSession))
+    if ($userSession['username'] == $_GET['u'])
+        $myPage = true;
+
 ?>
 <body>
     <?php
@@ -10,77 +23,82 @@ GenerateHeader('User Page', ['userPage.css']);
         <div class="info-container">
             <div class="gradient-banner"></div>
             <div class="user-container">
-                <h2>Fake User</h2>
+                <h2 id="username"></h2>
+                <hr />
                 <p>Your Quizzes</p>
-                <div class="quizzes-container">
-                    <a class="quiz-card" href="./editQuiz.php?quiz_id=" + id>
-                        <div class="quiz-body">
-                            <div class="quiz-title">Quiz Name</div>
-                            <p>Description</p>
-                        </div>
-                    </a>
-                    <a class="quiz-card">
-                        <div class="quiz-body">
-                            <div class="quiz-title">Quiz Name</div>
-                            <p>Description</p>
-                        </div>
-                    </a>
-                    <a class="quiz-card">
-                        <div class="quiz-body">
-                            <div class="quiz-title">Quiz Name</div>
-                            <p>Description</p>
-                        </div>
-                    </a>
-                    <a class="quiz-card">
-                        <div class="quiz-body">
-                            <div class="quiz-title">Quiz Name</div>
-                            <p>Description</p>
-                        </div>
-                    </a>
-                    <a class="quiz-card">
-                        <div class="quiz-body">
-                            <div class="quiz-title">Quiz Name</div>
-                            <p>Description</p>
-                        </div>
-                    </a>
-                </div>
+                <div class="quizzes-container"></div>
             </div>
         </div>
         <img class="profile-img"
             src="https://th.bing.com/th/id/R.6e0f2f6bab06c008c79a3b280db49342?rik=jsupiHsPJSF3lA&riu=http%3a%2f%2fassets0.prcdn.com%2fuk%2fpeople%2fdefault-profile.png%3f1406639312&ehk=fgzMewvFjuDc4jTAGFrQAJ%2fhZD3vLHtzxhxb%2bnle3cU%3d&risl=&pid=ImgRaw&r=0" />
     </div>
+    <input id="myPageHidden" type="hidden" value="<?php echo $myPage ?>" />
 </body>
-<script type="text/javascript">
-    var request = new XMLHttpRequest();
-    const address = "./backend/sqlSelect.php?table=quiz";
 
-    request.open("GET", address);
-    request.onload = onDataLoaded;
-    request.send();
+<script>
+    const isMyPage = document.getElementById('myPageHidden').value;
+    const getUserData = async () => {
+        // get username from url param
+        var urlFormatParams = window.location.search;
+        var params = new URLSearchParams(urlFormatParams);
+        var username = params.get('u');
 
-    const onDataLoaded = (event) => {
-        var data = event;
-        var response = request.responseText;    
-        data = JSON.parse(response);
-    }
+        // call api for that users info
+        const response = await fetch('backend/sqlJoin.php?join=profile', {
+            method: 'POST',
+            body: `{"username": "${username}"}`,
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const json = await response.json();
 
-    function generateQuizzes(data) {
-        for(var i in data) {
-            var request = new XMLHttpRequest();
-            const address = "quizCard.php?quizId=" + data[i].id + "&name=" + data[i].title + "&color=" + data[i].color + "&description=" + data[i].description;
+        // get username element
+        var headerUsername = document.getElementById('username');
+        headerUsername.innerHTML = username;
 
-            request.open("GET", address);
-            request.onload = updateQuizContainer;
-            request.send();
+        // check if there's a need to try populate
+        if (json == []) {
+            return;
         }
+
+        // get quiz element container
+        var container = document.getElementsByClassName('quizzes-container')[0];
+
+        // container for all html elements
+        let quizElements = [];
+        // for each row
+        json.forEach((data) => {
+            quizElements.push(QuizElement(data));
+        });
+
+        // if my page then add addquiz option
+        if (isMyPage) {
+            quizElements.push(`
+                <a class="quiz-card" href="./addQuiz.php">
+                    <div class="quiz-body">
+                        <div class="quiz-title">Create New Quiz</div>
+                    </div>
+                </a>`);
+        }
+
+        // apply to container
+        container.innerHTML = quizElements.join('\n');
     }
 
-    const updateQuizContainer = (event) => {
-        var data = event;
-        var response = request.responseText;    
-        data = JSON.parse(response);
+    const QuizElement = (data) => {
+        let quizElement = `
+                <div class="quiz-card">
+                    <div class="quiz-body">
+                        <div class="quiz-title">${data.title}</div>
+                        <div class="quiz-link-cont">
+                            <a href="./playQuiz.php?q=${data.id}">PLAY!</a>
+                            ${(isMyPage) ? `<a href="./editQuiz.php?quiz_id=${data.id}">EDIT!</a>` : ''}
+                        </div>
+                    </div>
+                </div>`;
 
-        document.getElementById("quizzes-container").innerHTML += data;
+        return quizElement;
     }
+
+    getUserData();
 </script>
 <?php
